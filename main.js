e8d581e191d01f7d5b035051e48f9cf501201b33
Keshav3003 <117528968+Keshav3003@users.noreply.github.com>
2023-07-18 03:04:56 +0530

    Fix: Happy Image not Showing and Score turned into next line

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function run() {
 
   const img = document.createElement('img');
   img.classList.add('mole');
-  img.src = 'assets/happy.png';
+  img.src = 'assets/Happy.png';
 
   img.addEventListener('click', () => {
     score += 10;
